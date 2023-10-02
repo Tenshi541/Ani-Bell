2480b4206248ad035cdb3122f3165724554f260b
Tenshi541 <angelzavaleta541@gmail.com>
2023-10-01 21:21:33 -0700

    app.css and home page

--- my-ani-bell/client/src/pages/Home/Home.js
+++ my-ani-bell/client/src/pages/Home/Home.js
@@ -1,7 +1,9 @@
+// Note: This is the Home page. It is the first page that the user sees when they visit the site.
+//       It is the default page that is loaded when the user visits the site.
 function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <h1>Ani-Bell</h1>
     </div>
   );
 }
