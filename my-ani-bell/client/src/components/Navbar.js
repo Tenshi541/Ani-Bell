42e15c60019066e3da317be7b29be86753a9c231
Tenshi541 <angelzavaleta541@gmail.com>
2023-10-16 23:43:44 -0700

    starting navbar again

--- my-ani-bell/client/src/components/Navbar.js
+++ my-ani-bell/client/src/components/Navbar.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import Auth from '../../utils/auth';
+import './style.css';
 
 
-function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
-  );
-}
 //i love andrea<3
 
-export default Navbar;
+const Navbar = () => {
